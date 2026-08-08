cf9c1814bab8db54225e2461bcbb4820f65a8665
Edson G Maldonado <edsingm@hotmail.com>
2026-08-08 12:37:56 -0300

    fix postgreSQL ci

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
-use LogicException;
 
 trait CreatesApplication
 {
@@ -12,18 +11,6 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
-
-        if (
-            ! $app->environment('testing')
-            || config('database.default') !== 'sqlite'
-            || config('database.connections.sqlite.database') !== ':memory:'
-            || config('tenancy.database.central_connection') !== 'sqlite'
-        ) {
-            throw new LogicException(
-                'Os testes devem usar APP_ENV=testing, conexão central SQLite e banco :memory:. '
-                .'Execute "composer test" para limpar o cache de configuração antes da suíte.'
-            );
-        }
 
         return $app;
     }
